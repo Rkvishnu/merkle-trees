2ff1ebf9bf738f4cf89ceaa42aa3ec376f8f9a60
Vishnu Rathore <vr8043836@gamil.com>
2022-08-17 18:28:41 +0530

    merkle-trees

---
+++ contracts/whitelist.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract Whitelist{
+    bytes32 public merkleRoot;
+    // we are not storing the address of each user in the contract, instead, 
+    //we are only storing the root of the merkle tree which gets initialized in the constructor.
+    constructor(bytes32 _merkleRoot){
+        merkleRoot= _merkleRoot;
+    }
+
+    function checkInWhitelist(bytes32[] calldata proof ,uint64 maxAllowanceToMint) view public returns(bool){
+        bytes32 leaf = keccak256(abi.encode(msg.sender,maxAllowanceToMint));
+       bool verified = MerkleProof.verify(proof,merkleRoot,leaf);
+       return verified;
+    }
+}
